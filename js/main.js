f302bdce7f28fff8a7a4854f47b1e74a53db9a27
Sobik Szymon <sobikszymon@imac-sobik.home>
2018-06-27 19:11:48 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    navBar();
+    navigation();
+    topShadow();
+    $('.dummy').viewportChecker();
+});
+
+$(window).scroll(function() {
+    topShadow();
+})
+
+function navBar() {
+    $('body').on('click', '.menuBtn', function() {
+        $('.navBox').removeClass('hide');
+    });
+    
+    $('body').on('click', '.menuBtnHide', function() {
+        $('.navBox').addClass('hide');
+    });
+}
+
+function navigation() {
+    $('body').on('click', '.scrollPage', function(e) {
+        var section = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(section).offset().top
+        });
+        $('.navBox').addClass('hide');
+        e.preventDefault();
+        return false;
+    });
+}
+
+function topShadow() { 
+    var scroll = $(window).scrollTop(),
+        pageHeight = $(window).outerHeight() - 40;
+
+    if (scroll <= pageHeight) {
+        $('.topBar').removeClass('shadow');
+    } else {
+        $('.topBar').addClass('shadow');
+    }
+}
