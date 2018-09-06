5346cea159e63d40dfb7f7f0ac86f5177fb55837
szymonsobik <sobikszymon@imac-sobik.home>
2018-09-06 22:48:47 +0200

    Poprawki + Materiały

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
     navBar();
     navigation();
     topShadow();
+    hideModal();
+    $('body').on('click', '.offerSeeMore a', function(e) {
+        e.preventDefault();
+        var $id = $(this).attr('data-id');
+        modal($id);
+    });
     // $('.dummy').viewportChecker();
 });
 
@@ -40,4 +46,31 @@ function topShadow() {
     } else {
         $('.topBar').addClass('shadow');
     }
+}
+
+function modal($id) {
+    $('.modalShadow').fadeIn();
+    $('#' + $id).fadeIn();
+    $('body').css({
+        'overflow-y':'hidden'
+    });
+    $('body').on('click', '.modalShadow, .modalContent', function(e) {
+        e.preventDefault();
+        $('.modalShadow').fadeOut();
+        $('#' + $id).fadeOut();
+        $('body').css({
+            'overflow-y':'scroll'
+        });
+    });
+}
+
+function hideModal() {
+    $('body').on('click', '.closeModal', function(e) {
+        e.preventDefault();
+        $('.modalShadow').fadeOut();
+        $('.modalWrapper').fadeOut();
+        $('body').css({
+            'overflow-y':'scroll'
+        });
+    });
 }
